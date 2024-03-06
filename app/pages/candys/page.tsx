d30f80ef56a284718116b0907d43801c967a55c6
joaoleandro <new.joaoleandro@gmail.com>
2024-03-06 13:13:39 -0300

    feat: página sobremesa

--- app/pages/candys/page.tsx
+++ app/pages/candys/page.tsx
@@ -1,8 +1,29 @@
+import Container from "@/components/Container";
+import CardProducts from "../_components/CardsProducts";
+import { stackProducts } from "@/app/utils/stackProducts";
+
 const Candys = () => {
+    const stackCandys = stackProducts[2].candy
     return ( 
-        <div>
-            <span>Página dos doces</span>
+        <Container>
+        <div className="flex flex-col">
+          <div className="mt-8">
+            {stackCandys?.map((product: any) => {
+              return (
+                <div key={product.name}>
+                  <CardProducts
+                    src={product.urlImg}
+                    alt={product.name}
+                    title={product.name}
+                    price={product.price}
+                  />
+                  <br />
+                </div>
+              );
+            })}
+          </div>
         </div>
+      </Container>
      );
 }
  

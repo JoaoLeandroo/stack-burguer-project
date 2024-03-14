d5f5bda43cf5e854dde47aeebf1dca300d6d27f2
joaoleandro <new.joaoleandro@gmail.com>
2024-03-14 15:41:37 -0300

    feat: mudanças na página outros

--- app/pages/others/page.tsx
+++ app/pages/others/page.tsx
@@ -1,10 +1,21 @@
-
+import Container from "@/components/Container";
+import Link from "next/link";
 const Others = () => {
-    return ( 
-        <div>
-            <span>Outras comidas</span>
-        </div>
-     );
-}
- 
+  return (
+    <Container>
+      <div className="h-[500px] flex items-center flex-col justify-center bg-[#000000a1] mt-2 rounded-xl">
+        <span className="text-2xl font-bold">
+          Catalogo indisponível no momento.
+        </span>
+        <Link
+          href={"/"}
+          className="text-slate-400 hover:text-slate-500 transition duration-300"
+        >
+          Retorne a página inicial
+        </Link>
+      </div>
+    </Container>
+  );
+};
+
 export default Others;

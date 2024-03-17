f3bfc6dce257d6b664f9cc26715ecb0cb69eed09
joaoleandro <new.joaoleandro@gmail.com>
2024-03-17 16:01:53 -0300

    feat: página de suporte

--- app/pages/support/page.tsx
+++ app/pages/support/page.tsx
@@ -2,8 +2,9 @@ import Container from "@/components/Container";
 const Support = () => {
   return (
     <Container>
-      <div>
+      <div className="h-[500px] text-center flex items-center flex-col justify-center bg-[#000000a1] mt-2 rounded-xl">
         <span className="text-5xl font-bold">Algum problema? contate-nos</span>
+        <span className="mt-2">Telefone: (99) 99999-9999</span>
       </div>
     </Container>
   );
